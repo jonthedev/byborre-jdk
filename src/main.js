743cadd9b496b5ce2d7117f9ce145d5018ba4b53
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-05-03 09:21:01 +0200

    feat: setup firebase with vue

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { VueFire } from 'vuefire'
+import { firebaseApp } from './firebase'
 
 const app = createApp(App)
+
+app.use(VueFire, {
+  // imported above but could also just be created here
+  firebaseApp,
+  modules: []
+})
 
 app.use(router)
 
