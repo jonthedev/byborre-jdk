a345f436a7aaaea21e8f059de84590c6caf0d8f3
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-05-04 15:01:12 +0200

    feat: add navbar markup to HomeView page
    style: add navbar styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,13 @@ const users = useCollection(collection(db, 'users'))
 </script>
 
 <template>
+  <nav
+    className="sticky top-0 z-10 h-16 shadow-sm shadow-slate-950"
+    id="navbar"
+    data-testid="navbar"
+  >
+    BYBORRE
+  </nav>
   <div class="background-color flex justify-center items-center h-full">
     <p class="text-4xl font-bold">
       Sign up successfull! Welcome to BYBORRE Create<span class="trademark">&trade;</span>
