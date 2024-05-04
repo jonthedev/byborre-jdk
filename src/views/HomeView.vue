85020fba78d45bffb3a23f5e6fb4d4a48585bfab
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-05-04 14:02:40 +0200

    style: make app container span full height of viewport
    style: add '#ccc' background color to tailwind theme
    
    feat: add welcome message to HomeView page
    
    style: add welcome message styles to fit figma design

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,19 +3,13 @@ import { useFirestore, useCollection } from 'vuefire'
 import { collection } from 'firebase/firestore'
 
 const db = useFirestore()
-
 const users = useCollection(collection(db, 'users'))
 </script>
 
 <template>
-  <div>
-    <h2>List of Users</h2>
-    <ul v-if="users.length > 0">
-      <li v-for="user in users" :key="user.id">
-        <div>{{ user.fullName }}</div>
-        <div>{{ user.email }}</div>
-      </li>
-    </ul>
-    <div v-else>No users found.</div>
+  <div class="background-color flex justify-center items-center h-full">
+    <p class="text-4xl font-bold">
+      Sign up successfull! Welcome to BYBORRE Create<span class="trademark">&trade;</span>
+    </p>
   </div>
 </template>
