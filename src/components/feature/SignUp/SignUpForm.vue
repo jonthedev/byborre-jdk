35ada38c0cc16e01cbe52dc7867312dc385dfebe
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-05-05 00:05:26 +0200

    feat(SignUpForm): use composition api to retrieve form data

--- src/components/feature/SignUp/SignUpForm.vue
+++ src/components/feature/SignUp/SignUpForm.vue
@@ -1,44 +1,36 @@
 <template>
   <div>
-    <h2>Sign Up</h2>
+    <h2>Sign up for Create <span class="trademark">&trade;</span></h2>
     <form @submit.prevent="submitForm">
       <div>
-        <label for="fullname">Full Name</label>
-        <input type="text" id="fullname" v-model="fullname" required />
+        <label for="fullName">Full Name</label>
+        <input type="text" id="fullName" v-model="newUser.fullName" required />
       </div>
       <div>
         <label for="email">Email</label>
-        <input type="email" id="email" v-model="email" required />
+        <input type="email" id="email" v-model="newUser.email" required />
       </div>
       <div>
         <label for="password">Password</label>
-        <input type="password" id="password" v-model="password" required />
+        <input type="password" id="password" v-model="newUser.password" required />
       </div>
-      <button type="submit">Sign Up</button>
+      <button type="submit" @click.prevent="submitForm">Sign Up</button>
     </form>
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      fullname: '',
-      email: '',
-      password: ''
-    }
-  },
-  methods: {
-    submitForm() {
-      // Here you can handle form submission
-      console.log('Full Name:', this.fullname)
-      console.log('Email:', this.email)
-      console.log('Password:', this.password)
-    }
-  }
+<script setup>
+import { ref } from 'vue'
+
+const newUser = ref({
+  fullName: '',
+  email: '',
+  password: ''
+})
+
+function submitForm() {
+  console.log('Full Name:', newUser.value.fullName)
+  console.log('Email:', newUser.value.email)
+  console.log('Password:', newUser.value.password)
 }
 </script>
-
-<style>
-/* Add your custom styles here */
-</style>
