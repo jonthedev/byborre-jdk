d6875fc61d11a7074541b523b5edfa63a2a309c1
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-05-05 12:58:08 +0200

    refactor(NavBar): move tailwind utility classes from markup into style tag

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,21 @@
 <template>
-  <nav
-    class="navbar absolute top-0 z-10 h-16 shadow-sm shadow-slate-950 px-6 flex items-center min-w-full"
-    id="navbar"
-    data-testid="navbar"
-  >
+  <nav class="navbar" id="navbar" data-testid="navbar">
     <div class="navbar__logo">
-      <h1 class="logo__text mr-auto font-extrabold text-xl tracking-wide relative">
-        BYBORRE<span class="logo__trademark absolute top-0.5 text-xs font-normal">&reg;</span>
-      </h1>
+      <h1 class="navbar__logo__text">BYBORRE<span class="logo__trademark">&reg;</span></h1>
     </div>
   </nav>
 </template>
+
+<style scoped>
+.navbar {
+  @apply absolute top-0 z-10 h-16 shadow-sm shadow-slate-950 px-6 flex items-center min-w-full;
+}
+
+.navbar__logo__text {
+  @apply mr-auto font-extrabold text-xl tracking-wide relative;
+}
+
+.logo__trademark {
+  @apply absolute top-0.5 text-xs font-normal;
+}
+</style>
